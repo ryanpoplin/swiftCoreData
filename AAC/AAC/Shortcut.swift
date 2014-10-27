f32dbb1e18bacbbbcb0bbc192c20eb37e3acc5b4
Byrdann Fox <mobileupstate@gmail.com>
2014-10-27 02:26:04 -0400

    Updates...

---
+++ AAC/AAC/Shortcut.swift
@@ -0,0 +1,16 @@
+//
+//  Shortcut.swift
+//  AAC
+//
+//  Created by Byrdann Fox on 10/27/14.
+//  Copyright (c) 2014 ExcepApps, Inc. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Shortcut: NSManagedObject {
+
+    @NSManaged var shortcut: String
+
+}
