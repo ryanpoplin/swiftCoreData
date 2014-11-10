ed2c7b5e7cc2d06cb169bbecf5a129c08e4872ca
Byrdann Fox <mobileupstate@gmail.com>
2014-11-09 20:51:06 -0500

    Updates...

--- AAC/AAC/text.js
+++ AAC/AAC/text.js
@@ -13,28 +13,33 @@ function analyzeText(text) {
     try {
         sentences -= textString.match(/\.\w\w?\./g).length;
     }
-    catch (er){}
+    catch (er) {
+        sentences = 1;
+    }
     try {
         sentences -= textString.match(/\s\w\w?\./g).length;
     }
-    catch (er){}
+    catch (er) {
+        sentences = 1;
+    }
     textString = textString.replace(/(\.\s*\w)/ig, function($0) { return $0.toLowerCase(); });
     var shorts;
     try {
         shorts = textString.match(/(can|don|it|wouldn|aren|he|she|i|you|we|won|didn|ain|isn|doesn)['â€˜â€™](t|s|m|re|ll)/ig).length;
-                                                                                                   }
-                                                                                                   catch (er) {
-                                                                                                   shorts = 0;
-                                                                                                   }
-                                                                                                   textString = textString.replace(/[,\.\\\/\(\)\:\"\';\[\]â€¦â€˜â€™â€žâ€œâ€Â«Â»â€”â€“-]/g, " ");
-                                                                                                                                     textString = textString.replace(/\s\s+/g, " ").replace(/^\s+|\s+$/g, "");
-                                                                                                                                     var wordsArray = textString.split(' ');
-                                                                                                                                     var wordsCount = wordsArray.length - shorts;
-                                                                                                                                     var wordsPerSentence = Math.round(wordsCount / sentences);
-                                                                                                                                     var totalWordsLength = 0;
-                                                                                                                                     for (var key in wordsArray)
-                                                                                                                                     totalWordsLength += wordsArray[key].length;
-                                                                                                                                     var averageWordLength = Math.round(totalWordsLength / wordsCount);
-                                                                                                                                     var dataArr = [sentences, wordsCount, wordsPerSentence, averageWordLength];
-                                                                                                                                     return dataArr;
-                                                                                                                                     }
+    } catch (er) {
+        shorts = 0;
+    }
+    textString = textString.replace(/[,\.\\\/\(\)\:\"\';\[\]â€¦â€˜â€™â€žâ€œâ€Â«Â»â€”â€“-]/g, " ");
+    textString = textString.replace(/\s\s+/g, " ").replace(/^\s+|\s+$/g, "");
+    var wordsArray = textString.split(' ');
+    if (sentences === 0)
+        sentences = 1;
+    var wordsCount = wordsArray.length - shorts;
+    var wordsPerSentence = Math.round(wordsCount / sentences);
+    var totalWordsLength = 0;
+    for (var key in wordsArray)
+        totalWordsLength += wordsArray[key].length;
+    var averageWordLength = Math.round(totalWordsLength / wordsCount);
+    var dataArr = [sentences, wordsCount, wordsPerSentence, averageWordLength];
+    return dataArr;
+}
