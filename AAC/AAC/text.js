392bb5165426db9a53277ac2fddb2f6aa2a0fb20
Byrdann Fox <mobileupstate@gmail.com>
2014-12-06 13:27:37 -0500

    Updates...

--- AAC/AAC/text.js
+++ AAC/AAC/text.js
@@ -1,8 +1,9 @@
 var sentences, wordsCount, wordsPerSentence, averageWordLength;
-function analyzeText(text) {
+function analyzeText (text) {
     var textString = text;
-    if (textString.replace(/\s*/g,'').length == 0)
+    if (textString.replace(/\s*/g,'').length == 0) {
         return;
+    }
     textString = textString.replace(/(\.\.\.|!|\?)/g, ".").replace(/\s*$/g, "");
     try {
         sentences = textString.match(/\./g).length + (textString.charAt(textString.length-1) === '.'?0:1);
@@ -22,34 +23,31 @@ function analyzeText(text) {
     var shorts;
     try {
         shorts = textString.match(/(can|don|it|wouldn|aren|he|she|i|you|we|won|didn|ain|isn|doesn)['â€˜â€™](t|s|m|re|ll)/ig).length;
-                                                                                                   } catch (er) {
-                                                                                                   shorts = 0;
-                                                                                                   }
-                                                                                                   textString = textString.replace(/[,\.\\\/\(\)\:\"\';\[\]â€¦â€˜â€™â€žâ€œâ€Â«Â»â€”â€“-]/g, " ");
-                                                                                                                                     textString = textString.replace(/\s\s+/g, " ").replace(/^\s+|\s+$/g, "");
-                                                                                                                                     if (sentences === 0)
-                                                                                                                                     sentences = 1;
-                                                                                                                                     var wordsArray = textString.split(' ');
-                                                                                                                                     wordsCount = wordsArray.length - shorts;
-                                                                                                                                     wordsPerSentence = Math.round(wordsCount / sentences);
-                                                                                                                                     var totalWordsLength = 0;
-                                                                                                                                     for (var key in wordsArray)
-                                                                                                                                     totalWordsLength += wordsArray[key].length;
-                                                                                                                                     averageWordLength = Math.round(totalWordsLength / wordsCount);
-                                                                                                                                     }
-                                                                                                                                     
-                                                                                                                                     function getSentences() {
-                                                                                                                                     return sentences;
-                                                                                                                                     }
-                                                                                                                                     
-                                                                                                                                     function getWordsCount() {
-                                                                                                                                     return wordsCount;
-                                                                                                                                     }
-                                                                                                                                     
-                                                                                                                                     function getWordsPerSentence() {
-                                                                                                                                     return wordsPerSentence;
-                                                                                                                                     }
-                                                                                                                                     
-                                                                                                                                     function getAverageWordLength() {
-                                                                                                                                     return averageWordLength;
-                                                                                                                                     }
+    } catch (er) {
+        shorts = 0;
+    }
+    textString = textString.replace(/[,\.\\\/\(\)\:\"\';\[\]â€¦â€˜â€™â€žâ€œâ€Â«Â»â€”â€“-]/g, " ");
+    textString = textString.replace(/\s\s+/g, " ").replace(/^\s+|\s+$/g, "");
+    var wordsArray = textString.split(' ');
+    wordsCount = wordsArray.length - shorts;
+    if (sentences === 0) {
+        sentences = 1;
+    }
+    wordsPerSentence = Math.round(wordsCount / sentences);
+    var totalWordsLength = 0;
+    for (var key in wordsArray)
+        totalWordsLength += wordsArray[key].length;
+    averageWordLength = Math.round(totalWordsLength / wordsCount);
+}
+function getSentences () {
+    return sentences;
+}
+function getWordsCount () {
+    return wordsCount;
+}
+function getWordsPerSentence () {
+    return wordsPerSentence;
+}
+function getAverageWordLength() {
+    return averageWordLength;
+}
